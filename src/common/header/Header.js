fa06ed518245c8b1aefff4048a7361f5d0a5ae1b
ARUNKUMAR5008 <marun64@gmail.com>
2022-04-13 18:21:44 +0530

    check point 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css';
+import logo from './logo.svg';
+
+class Header extends React.Component {
+
+    render(){
+        return (
+            <div className="headerDiv">
+               <img src={logo} className="logoImg" alt="logo" />
+            </div>
+        )
+    }
+
+}
+
+export default Header;

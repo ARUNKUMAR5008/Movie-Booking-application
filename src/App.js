01e1261f968b19af20d40743c2277be833a1dc03
ARUNKUMAR5008 <marun64@gmail.com>
2022-04-24 22:37:32 +0530

    check point 5 added Details Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./common/header/Header";
+import Home from "./screens/home/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,17 +1,30 @@
 import React from "react";
 import './Header.css';
 import logo from '../../assets/logo.svg';
+import Button from "@material-ui/core/Button";
 
 class Header extends React.Component {
-
-    render(){
-        return (
-            <div className="headerDiv">
-               <img src={logo} className="logoImg" alt="logo" />
+    render() {
+      return (
+        <div className="headerDiv">
+          <img
+            src={logo}
+            alt="logo"
+            className="logoImg"
+          />
+  
+          {this.props.showBookShowButton === "true" ? (
+            <div className="bookshow-button">
+              <Button variant="contained" color="primary">
+                Book Show
+              </Button>
             </div>
-        )
+          ) : (
+            ""
+          )}
+        </div>
+      );
     }
-
-}
+  }
 
 export default Header;
